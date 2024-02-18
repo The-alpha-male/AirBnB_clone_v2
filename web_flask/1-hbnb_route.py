3d8046a35edbf1cb6bd752229c8ebd3cf21253b7
The-alpha-male <ronnymunene@outlook.com>
2024-02-18 12:42:01 -0800

    updated code

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Start a Flask web applicaton"""
+"""
+start Flask application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Routing to root, strict_slashes ensure
-    the URL works when it ends both with or without the /
-    """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Routing to /hbnb, strict_slashes ensure
-    the URL works when it ends both with or without the /
-    """
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
